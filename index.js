7c7769c5fba99c0783cdc200433d33638aa3c5c1
Lucas Chitolina <lucas.chitolina01@gmail.com>
2021-02-07 14:23:30 -0300

    last changes; next: styles

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 function takeshot() { 
+    // document.getElementById("slider").style.width = "75px";
+    // document.getElementById("slider").style.height = "15px";
+
     let div = document.getElementById('photo'); 
     let blank = document.getElementById('blank'); 
 
      html2canvas(div).then( 
-         
         function (canvas) {
             blank.classList.add("disappear");
             document.getElementById('output').appendChild(canvas); 
     }) 
 } 
 
+function updateUi(){
+    let slidervalue = document.getElementById("input-percentage").value;
+    
+    console.log(slidervalue);
+
+    document.getElementById("output-p").value = slidervalue;
+    document.getElementById("slider").style.width = `${slidervalue}px`;
+}
